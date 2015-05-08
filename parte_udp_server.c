7b8f87447924b0492026b2186184d1fe13c1f6ba
Alexandra Lomotan <alexandralomotan@gmail.com>
2015-05-08 16:24:24 -0700

    in progress

--- parte_udp_server.c
+++ parte_udp_server.c
@@ -3,9 +3,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+struct timeval slotTime;
+
 int main(int argc, char *argv[])
 {
     int sockfd, port;
+    int* csock;
     struct sockaddr_in serv_addr, cli_addr;
     int n;
     if (argc < 2) {
@@ -18,41 +21,63 @@ int main(int argc, char *argv[])
         exit(1);
     }
     port = atoi(argv[1]);
-
+    
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
-
+    
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         fprintf(stderr,"ERROR on binding\n");
         exit(1);
     }
-
+    
     socklen_t clilen = sizeof(cli_addr);
-
+    pthread_t thread_id=0;
+    
+    while (true) {
+        csock = (int*)malloc(sizeof(int));
+        if((*csock = accept(sockfd, (sockaddr*)&serv_addr, &clilen))!= -1){
+            printf("Received connection from %s\n",inet_ntoa(sadr.sin_addr));
+            pthread_create(&thread_id,0,&SocketHandler, (void*)csock);
+            pthread_detach(thread_id);
+        }
+        else{
+            fprintf(stderr, "Error accepting %d\n", errno);
+        }
+    }
+    
+    
+    
     // int rounds = 6;
-
+    
     // int KB[6] = {1, 4, 8, 16, 32, 64};
     char message[64000];
-
+    
     int i;
+    time_t t1, t2;
+    int switch = 0;
+    
+    //set slot times
+    slotTime.tv_sec = 0;
+    slotTime.tv_usec = 800;
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &slotTime, sizeof(slotTime));
+    
     while (strlen(message) != 42) {
         memset(&message, 0, strlen(message));
-    // printf("About to realloc to %2dKB\n", KB[i]);
-    // realloc(message, KB[i] * 1024);
-    // printf("Realloc'd successfully\n");
+        // printf("About to realloc to %2dKB\n", KB[i]);
+        // realloc(message, KB[i] * 1024);
+        // printf("Realloc'd successfully\n");
         n = recvfrom(sockfd, message, sizeof(message), 0, (struct sockaddr *)&cli_addr, &clilen);
+        
         if (n < 0) {
             fprintf(stderr,"ERROR reading from socket\n");
             exit(1);
         }
-        //get time received; check if within 1 time slot
-        //if within 1 time slot, collision. else success
+        
         printf("Received strlen(message) = %d\n", strlen(message));
-
         // printf("Received %2dKB, cycle %3d\n", KB[i], j);
         // printf("Here is the message: %s\n", message);
-
+        
         n = sendto(sockfd, message, strlen(message), 0, (struct sockaddr *)&cli_addr, clilen);
         // printf("Sent %2dKB, cycle %3d\n", strlen(message), j);
         if (n < 0) {
@@ -60,7 +85,7 @@ int main(int argc, char *argv[])
             exit(1);
         }
     }
-
+    
     close(sockfd);
     return 0;
 }
